6b1a194797c7b1d397e7a3fd6eb28686e508f343
Caian R. Ertl <caianrais@protonmail.com>
2018-06-27 04:07:43 -0300

    docs: include function documentation

--- as/main.js
+++ as/main.js
@@ -1,8 +1,46 @@
+/**
+ * @project no-candy-for-u (ncfu)
+ * @author Caian R. Ertl
+ * @license MIT
+ * @repository https://github.com/caianrais/ncfu
+ */
+
+
+// The web service endpoint/url
 var WS_URL = 'https://your-app-here.herokuapp.com/';
 
 
 /**
+ * Pauses the execution
  *
+ * Interrupts the routine execution for a given number of seconds
+ *
+ * @param {Integer} secs - The number of seconds
+ */
+function wait(secs) {
+    Utilities.sleep(secs * 1000);
+}
+
+/**
+ * Consumes the web service
+ *
+ * Makes a GET request to the web service in a given resource name
+ *
+ * @param {String} resource - The resource name
+ * @return {Object} The webservice response
+ */
+function consume(resource) {
+    return UrlFetchApp.fetch(WS_URL.concat(resource));
+}
+
+/**
+ * Notifies an event via email
+ *
+ * Sends an email message when an event occurs, i.e. the transition of an issue
+ * from "in progress" to "todo" or an uncaught exception on the web service.
+ *
+ * @param {String} status - The status of the event.
+ * @param {Object} content - The object containing context information about the event.
  */
 function notify(status, content) {
 
@@ -46,21 +84,12 @@ function notify(status, content) {
 }
 
 /**
+ * Triggers the web service
  *
- */
-function wait(secs) {
-    Utilities.sleep(secs * 1000);
-}
-
-/**
- *
- */
-function consume(resource) {
-    return UrlFetchApp.fetch(WS_URL.concat(resource));
-}
-
-/**
- *
+ * Sends a GET request to the '/work' resource. If one or more issue were left
+ * behind, the user will be notified. If something went wrong, the function will
+ * retry three times, with 30 seconds intervals. If it continues to fail, the user
+ * will be notified as well.
  */
 function activate() {
 
@@ -95,7 +124,10 @@ function activate() {
 }
 
 /**
+ * Pings the web service
  *
+ * Sends a GET request to the '/ping' resource in order to maintain the heroku
+ * service "warm" (since it's tend to sleep after 30 min or so).
  */
 function warm() {
     consume('ping');
